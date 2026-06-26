67fd8cb67b67d0e844d909817446d5f8a79befbd
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-06-27 01:54:54 +0600

    fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,9 +3,8 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URL);
-await client.connect();
-const db = client.db("legalease");
 
+// await সরিয়ে দিন - mongodbAdapter নিজেই connection handle করে
 export const auth = betterAuth({
   emailAndPassword: { 
     enabled: true, 
@@ -16,7 +15,7 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_SECRET_KEY, 
     }, 
   }, 
-  database: mongodbAdapter(db, { client }),
+  database: mongodbAdapter(client.db("legalease")),
   user: {
     additionalFields: {
       role: {
