4322c1ab4dde4d1c1e65563c1060e2f95b96163a
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-06-27 11:05:58 +0600

    fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
+ import { jwt } from "better-auth/plugins";
 const client = new MongoClient(process.env.MONGODB_URL);
 
 export const auth = betterAuth({
@@ -27,5 +27,17 @@ export const auth = betterAuth({
         default: "user"
       }
     }
-  }
+  },
+
+   session:{
+       cookieCache:{
+        maxAge:7*24*60*60,
+        strategy:"jwt",
+        enabled:true,
+        
+       }
+   },
+ plugins:[
+  jwt()
+ ]
 });
